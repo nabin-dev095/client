d71be29c17d1f9e3f0112912219c4d289f74d850
unknown <nabinmukhiya71765@gmail.com>
2026-08-12 15:03:33 +0545

    design login page using tailwind css

---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Metadata } from 'next';
+
+   
+export const metadata: Metadata = {
+  title: "Products | Nepali Pasal",
+  description: "All Products",
+  
+};
+
+const ProductViewPage = () => {
+  return (
+    <main>
+      <div>Product View Page</div>
+    </main>
+  );
+};
+
+export default ProductViewPage;
---
+++ app/products/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Metadata } from 'next';
+
+   
+export const metadata: Metadata = {
+  title: "Products | Nepali Pasal",
+  description: "All Products",
+  
+};
+
+const Products = () => {
+  return (
+    <main>
+      <div>Products</div>
+    </main>
+  );
+};
+
+export default Products;
