9f9bcbaf5eb4a52e4582a0bfac2cce1643fc9f18
unknown <nabinmukhiya71765@gmail.com>
2026-08-18 12:05:21 +0545

    form validation using yup

--- components/common/form/login.form.tsx
+++ components/common/form/login.form.tsx
@@ -4,27 +4,33 @@ import Input from "../ui/input";
 import Button from "../ui/button";
 import { useForm } from "react-hook-form";
 import { register } from "module";
-import * as yup from 'yup'
-import {yupResolver } from "@hookform/resolvers/yup";
-
-
-//* login Schema
-const loginSchema = yup.object({
-  email: yup.string().email('invalid email format').required('email is required'),
-  password: yup.string().required('password is required')
-})
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { loginSchema } from "@/schema/auth.schema";
+import { TLogin } from "@/types/auth.types";
+import { Login } from "@/api/auth.api";
 
 const LoginForm = () => {
-  const { register, handleSubmit , formState: {errors} } = useForm({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TLogin>({
     defaultValues: {
       email: "",
       password: "",
     },
-    resolver: yupResolver(loginSchema)
+    resolver: yupResolver(loginSchema),
   });
 
-  const onSubmit = (data: { email: string; password: string }) => {
-    console.log("Login form submitted", data);
+  const onSubmit = async (data: TLogin) => {
+    try {
+      console.log("Login form submitted", data);
+      const response = await Login(data);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
